cf5b65bb82694cf047a45e1643b17b1e984bf30f
yasuto88 <yasuto010788@gmail.com>
2024-05-22 19:13:58 +0900

    feat: Add populationComposition module with initial state
    都道府県を取得し、状態に保存する機能を追加

---
+++ population-visualization/src/reducks/populationComposition/slices.ts
@@ -0,0 +1,33 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { PopulationCompositionResponse } from "./types";
+import { initialState } from "./initializes";
+
+const populationSlice = createSlice({
+  name: "population",
+  initialState,
+  reducers: {
+    fetchPopulationStart(state) {
+      state.loading = true;
+      state.error = null;
+    },
+    fetchPopulationSuccess(
+      state,
+      action: PayloadAction<PopulationCompositionResponse>
+    ) {
+      state.loading = false;
+      state.data = action.payload;
+    },
+    fetchPopulationFailure(state, action: PayloadAction<string>) {
+      state.loading = false;
+      state.error = action.payload;
+    },
+  },
+});
+
+export const {
+  fetchPopulationStart,
+  fetchPopulationSuccess,
+  fetchPopulationFailure,
+} = populationSlice.actions;
+
+export default populationSlice.reducer;
