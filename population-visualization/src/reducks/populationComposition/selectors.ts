6bafde43dfa8df763b005a53589477f6adfb7fa9
yasuto88 <yasuto010788@gmail.com>
2024-05-22 19:13:58 +0900

    feat: Add populationComposition module with initial state
    都道府県を取得し、状態に保存する機能を追加

---
+++ population-visualization/src/reducks/populationComposition/selectors.ts
@@ -0,0 +1,7 @@
+import { RootState } from "../store";
+
+export const selectPopulation = (state: RootState) => state.population.data;
+export const selectPopulationLoading = (state: RootState) =>
+  state.population.loading;
+export const selectPopulationError = (state: RootState) =>
+  state.population.error;
