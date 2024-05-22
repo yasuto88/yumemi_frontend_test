a826b583186dfc08c32097e445f8de33a43b2b0a
yasuto88 <yasuto010788@gmail.com>
2024-05-22 17:56:42 +0900

    chore: Add prefecture list feature to Redux store
    都道府県リスト取得機能をReduxストアに追加

--- population-visualization/src/reducks/prefectureList/selectors.ts
+++ population-visualization/src/reducks/prefectureList/selectors.ts
@@ -1,4 +1,8 @@
+<<<<<<< HEAD
 import { RootState } from '../store';
+=======
+import { RootState } from "../store";
+>>>>>>> 78a6833 (chore: Add prefecture list feature to Redux store)
 
 export const selectPrefectures = (state: RootState) => state.prefectures.list;
 export const selectPrefecturesLoading = (state: RootState) =>
