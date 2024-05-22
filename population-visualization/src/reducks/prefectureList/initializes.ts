436bc75767321ddaa61b92311441f858b65f7e3d
yasuto88 <yasuto010788@gmail.com>
2024-05-22 17:55:50 +0900

    add: Add initial state for prefecture list feature
    都道府県の状態と取得状態を追加

---
+++ population-visualization/src/reducks/prefectureList/initializes.ts
@@ -0,0 +1,10 @@
+import { PrefecturesState } from "./types";
+
+export const initialState: PrefecturesState = {
+  list: [
+    { prefCode: 1, prefName: "Hokkaido" },
+    { prefCode: 2, prefName: "Aomori" },
+  ],
+  loading: false,
+  error: null,
+};
