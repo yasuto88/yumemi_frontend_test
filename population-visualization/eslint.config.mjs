e6ac9b45bd87454be5752ac6e387c5c0bcd65da3
yasuto88 <yasuto010788@gmail.com>
2024-05-22 16:15:29 +0900

    fix: Update eslint configuration and integrate prettier

--- population-visualization/eslint.config.mjs
+++ population-visualization/eslint.config.mjs
@@ -1,43 +1,12 @@
 import globals from 'globals';
 import pluginJs from '@eslint/js';
-import tseslint from '@typescript-eslint/eslint-plugin';
+import tseslint from 'typescript-eslint';
 import pluginReactConfig from 'eslint-plugin-react/configs/recommended.js';
 import { fixupConfigRules } from '@eslint/compat';
-import prettierConfig from 'eslint-config-prettier';
-import pluginPrettier from 'eslint-plugin-prettier';
 
 export default [
-  {
-    languageOptions: {
-      globals: { ...globals.browser, ...globals.node },
-      parser: '@typescript-eslint/parser',
-      parserOptions: {
-        ecmaVersion: 2020,
-        sourceType: 'module',
-        ecmaFeatures: {
-          jsx: true,
-        },
-      },
-    },
-    settings: {
-      react: {
-        version: 'detect',
-      },
-    },
-  },
+  { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
   pluginJs.configs.recommended,
-  tseslint.configs.recommended,
-  fixupConfigRules(pluginReactConfig),
-  prettierConfig,
-  {
-    plugins: {
-      prettier: pluginPrettier,
-    },
-    rules: {
-      'prettier/prettier': 'error',
-      'react/react-in-jsx-scope': 'off', // React 17以降では必要ないため
-      '@typescript-eslint/explicit-module-boundary-types': 'off',
-      '@typescript-eslint/no-explicit-any': 'off',
-    },
-  },
+  ...tseslint.configs.recommended,
+  ...fixupConfigRules(pluginReactConfig),
 ];
