332789d31b7a74267fa47759d9194d53e3b4d49e
yasuto88 <yasuto010788@gmail.com>
2024-05-22 16:32:57 +0900

    fix: Update eslint configuration and integrate prettier

--- population-visualization/eslint.config.mjs
+++ population-visualization/eslint.config.mjs
@@ -1,12 +1,28 @@
-import globals from 'globals';
-import pluginJs from '@eslint/js';
-import tseslint from 'typescript-eslint';
-import pluginReactConfig from 'eslint-plugin-react/configs/recommended.js';
-import { fixupConfigRules } from '@eslint/compat';
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
+import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
+import { fixupConfigRules } from "@eslint/compat";
 
 export default [
-  { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-  ...fixupConfigRules(pluginReactConfig),
+  {
+    languageOptions: {
+      parser: tsParser,
+      globals: { ...globals.browser, ...globals.node },
+    },
+    rules: {
+      // ここにすべてのルールを記述
+    },
+    plugins: {
+      ...pluginJs.configs.recommended.plugins,
+      ...tseslint.configs.recommended.plugins,
+      ...fixupConfigRules(pluginReactConfig).plugins,
+    },
+    settings: {
+      ...pluginJs.configs.recommended.settings,
+      ...tseslint.configs.recommended.settings,
+      ...fixupConfigRules(pluginReactConfig).settings,
+    },
+  },
 ];
