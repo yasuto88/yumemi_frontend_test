bf3e598acd3feaea846ded05e7073e9c2bc66d9b
yasuto88 <119556268+yasuto88@users.noreply.github.com>
2024-05-24 08:14:25 +0900

    Revert "Feature/redux"

--- population-visualization/src/reducks/prefectureList/selectors.ts
+++
@@ -1,7 +0,0 @@
-import { RootState } from '../store';
-
-export const selectPrefectures = (state: RootState) => state.prefectures.list;
-export const selectPrefecturesLoading = (state: RootState) =>
-  state.prefectures.loading;
-export const selectPrefecturesError = (state: RootState) =>
-  state.prefectures.error;
