c36e74ec6695605b25998a28903968387d3d6526
yasuto88 <yasuto010788@gmail.com>
2024-05-23 15:58:22 +0900

    fix: format with prettier

--- population-visualization/src/reducks/prefectureList/initializes.ts
+++ population-visualization/src/reducks/prefectureList/initializes.ts
@@ -1,9 +1,9 @@
-import { PrefecturesState } from "./types";
+import { PrefecturesState } from './types';
 
 export const initialState: PrefecturesState = {
   list: [
-    { prefCode: 1, prefName: "Hokkaido" },
-    { prefCode: 2, prefName: "Aomori" },
+    { prefCode: 1, prefName: 'Hokkaido' },
+    { prefCode: 2, prefName: 'Aomori' },
   ],
   loading: false,
   error: null,
--- population-visualization/src/reducks/prefectureList/operations.ts
+++ population-visualization/src/reducks/prefectureList/operations.ts
@@ -1,22 +1,22 @@
-import { AppThunk } from "../store";
+import { AppThunk } from '../store';
 import {
   fetchPrefecturesStart,
   fetchPrefecturesSuccess,
   fetchPrefecturesFailure,
-} from "./slices";
-import { Prefecture } from "./types";
+} from './slices';
+import { Prefecture } from './types';
 
 const REACT_APP_API_KEY = process.env.REACT_APP_API_KEY;
 
 export const fetchPrefectures = (): AppThunk => async (dispatch) => {
   try {
-    console.log("API_KEY:", REACT_APP_API_KEY);
+    console.log('API_KEY:', REACT_APP_API_KEY);
     dispatch(fetchPrefecturesStart());
     const response = await fetch(
-      "https://opendata.resas-portal.go.jp/api/v1/prefectures",
+      'https://opendata.resas-portal.go.jp/api/v1/prefectures',
       {
-        headers: { "X-API-KEY": REACT_APP_API_KEY ?? "" },
-      }
+        headers: { 'X-API-KEY': REACT_APP_API_KEY ?? '' },
+      },
     );
     const data = await response.json();
     const prefectures: Prefecture[] = data.result;
