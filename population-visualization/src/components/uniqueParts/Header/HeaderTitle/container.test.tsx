1b61f81355f58bdbed3290cad458c437198d9506
yasuto88 <yasuto010788@gmail.com>
2024-05-23 17:56:56 +0900

    feat: Update HeaderTitleContainer component with dynamic title
    ヘッダータイトルのコンテナコンポーネントを動的なタイトルに更新

--- population-visualization/src/components/uniqueParts/Header/HeaderTitle/container.test.tsx
+++ population-visualization/src/components/uniqueParts/Header/HeaderTitle/container.test.tsx
@@ -1,11 +1,11 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import HeaderTitleContainer from './container';
 
 describe('HeaderTitleContainer', () => {
   it('renders the HeaderTitlePresentational with the correct title', () => {
-    const { getByText } = render(<HeaderTitleContainer />);
+    render(<HeaderTitleContainer />);
     
     // タイトルが正しくレンダリングされているか確認
-    expect(getByText('都道府県別の総人口推移グラフ')).toBeInTheDocument();
+    expect(screen.getByText('都道府県別の総人口推移グラフ')).toBeInTheDocument();
   });
 });
--- population-visualization/src/components/uniqueParts/Header/HeaderTitle/presenter.test.tsx
+++ population-visualization/src/components/uniqueParts/Header/HeaderTitle/presenter.test.tsx
@@ -1,22 +1,19 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import HeaderTitle, { ContainerProps } from './presenter';
+import { render, screen } from '@testing-library/react';
+import HeaderTitle from './presenter';
 
 describe('HeaderTitle', () => {
   it('renders the title text correctly', () => {
     const title = 'Test Title';
-    const { getByText } = render(<HeaderTitle title={title} />);
-    const headerElement = getByText(title);
+    render(<HeaderTitle title={title} />);
+    const headerElement = screen.getByText(title);
     expect(headerElement).toBeInTheDocument();
   });
 
   it('applies the passed className', () => {
     const title = 'Test Title';
     const className = 'custom-class';
-    const { container } = render(
-      <HeaderTitle title={title} className={className} />,
-    );
-    const headerElement = container.querySelector('h1');
+    render(<HeaderTitle title={title} className={className} />);
+    const headerElement = screen.getByRole('heading', { name: title });
     expect(headerElement).toHaveClass('header-title');
     expect(headerElement).toHaveClass(className);
   });
