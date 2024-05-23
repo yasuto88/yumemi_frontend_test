d89706b4f28f44f3c26b504846ab0c6174e8da03
yasuto88 <yasuto010788@gmail.com>
2024-05-23 16:26:44 +0900

    feat: Update HeaderTitleContainer component with static title
    ヘッダータイトルのコンテナコンポーネントを静的なタイトルに更新

--- population-visualization/src/components/uiParts/header/HeaderTitle/container.tsx
+++ population-visualization/src/components/uiParts/header/HeaderTitle/container.tsx
@@ -1,18 +1,8 @@
-import { useTitle } from './hooks';
 import { ContainerProps } from './presenter';
 import HeaderTitlePresentational from './presenter';
 
-const getTitle = async () => {
-  // 仮のタイトル取得関数
-  return new Promise<string>((resolve) =>
-    setTimeout(() => resolve('取得したタイトル'), 0),
-  );
-};
-
 const HeaderTitleContainer: React.FC<ContainerProps> = (props) => {
-  // TODO: 仮のタイトル取得関数を実際の関数に差し替える
-  const title = useTitle(getTitle);
-  return <HeaderTitlePresentational {...props} title={title} />;
+  return <HeaderTitlePresentational {...props} title='都道府県別の総人口推移グラフ' />;
 };
 
 export default HeaderTitleContainer;
--- population-visualization/src/components/uiParts/header/HeaderTitle/index.ts
+++ population-visualization/src/components/uiParts/header/HeaderTitle/index.ts
@@ -1,1 +1,1 @@
-export { default as Button } from './container';
+export { default as HeaderTitleContainer } from './container';
