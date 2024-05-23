3a49b24cff501184cf1cfb06c254d82b09175b65
yasuto88 <yasuto010788@gmail.com>
2024-05-23 15:54:17 +0900

    feat: Add Home component and update App component
    ホームコンポーネントを追加し、Appコンポーネントを更新

---
+++ population-visualization/src/components/pages/home.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+interface Props {
+  // props here
+}
+
+// 仮のページコンポーネント
+const Component: React.FC<Props> = (props) => {
+  return (
+    <></>
+  );
+};
+
+export default Component;
