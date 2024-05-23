a999c57aaeb9093b17ae75d054e04f48ea02c7a6
yasuto88 <yasuto010788@gmail.com>
2024-05-23 17:58:15 +0900

    feat: Update HeaderTitleContainer component with dynamic title

--- population-visualization/src/components/uniqueParts/Header/HeaderTitle/container.tsx
+++ population-visualization/src/components/uniqueParts/Header/HeaderTitle/container.tsx
@@ -2,7 +2,12 @@ import { ContainerProps } from './presenter';
 import HeaderTitlePresentational from './presenter';
 
 const HeaderTitleContainer: React.FC<ContainerProps> = (props) => {
-  return <HeaderTitlePresentational {...props} title='都道府県別の総人口推移グラフ' />;
+  return (
+    <HeaderTitlePresentational
+      {...props}
+      title="都道府県別の総人口推移グラフ"
+    />
+  );
 };
 
 export default HeaderTitleContainer;
