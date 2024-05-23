219ff008561bb2c58bf4d8e2105137db258e64ae
yasuto88 <119556268+yasuto88@users.noreply.github.com>
2024-05-24 08:12:11 +0900

    Revert "Feature/create header component"

--- population-visualization/src/components/uniqueParts/Header/container.tsx
+++
@@ -1,8 +0,0 @@
-import { ContainerProps } from './presenter';
-import HeaderPresentational from './presenter';
-
-const HeaderContainer: React.FC<ContainerProps> = (props) => {
-  return <HeaderPresentational {...props} />;
-};
-
-export default HeaderContainer;
