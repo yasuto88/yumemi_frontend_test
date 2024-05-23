b939dd8f89a801efc0a39860bc4017a173987560
yasuto88 <yasuto010788@gmail.com>
2024-05-23 16:00:18 +0900

    fix: format with prettier

---
+++ population-visualization/src/components/pages/home.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+interface Props {
+  // props here
+}
+
+// 仮のページコンポーネント
+const Component: React.FC<Props> = (props) => {
+  return <></>;
+};
+
+export default Component;
