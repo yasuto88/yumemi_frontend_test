092361933de5431a10ba40bdf1c5e9b090c61556
yasuto88 <yasuto010788@gmail.com>
2024-05-24 12:19:37 +0900

    chore: Update eslint.config.mjs and package-lock.json for eslint-plugin-testing-library v6.2.2

--- population-visualization/eslint.config.mjs
+++ population-visualization/eslint.config.mjs
@@ -3,26 +3,30 @@ import pluginJs from '@eslint/js';
 import tseslint from '@typescript-eslint/eslint-plugin';
 import tsParser from '@typescript-eslint/parser';
 import pluginReactConfig from 'eslint-plugin-react/configs/recommended.js';
+import testingLibrary from 'eslint-plugin-testing-library';
 import { fixupConfigRules } from '@eslint/compat';
 
-export default [
+const config = [
   {
     languageOptions: {
       parser: tsParser,
-      globals: { ...globals.browser, ...globals.node },
+      globals: { ...globals.browser, ...globals.node }
     },
     rules: {
-      // ここにすべてのルールを記述
+      'testing-library/no-node-access': ['error', { allowContainerFirstChild: true }]
     },
     plugins: {
       ...pluginJs.configs.recommended.plugins,
       ...tseslint.configs.recommended.plugins,
       ...fixupConfigRules(pluginReactConfig).plugins,
+      'testing-library': testingLibrary
     },
     settings: {
       ...pluginJs.configs.recommended.settings,
       ...tseslint.configs.recommended.settings,
-      ...fixupConfigRules(pluginReactConfig).settings,
-    },
-  },
+      ...fixupConfigRules(pluginReactConfig).settings
+    }
+  }
 ];
+
+export default config;
