a3cafdc8d2e2c88fffc3961896c768cff8941afa
yasuto88 <yasuto010788@gmail.com>
2024-05-24 13:31:50 +0900

    feat: Move CheckBox component and container to new directory チェックボックスをラップするコンポーネントのUIとビジネスロックを作成

---
+++ population-visualization/src/components/pages/CheckBoxGroup/hooks.ts
@@ -0,0 +1,29 @@
+import { useSelector } from 'react-redux';
+import { RootState } from '../../../reducks/store';
+import {
+  selectPrefectures,
+  selectPrefecturesError,
+  selectPrefecturesLoading,
+} from '../../../reducks/prefectureList/selectors';
+
+/**
+ * 都道府県リストの状態を監視するカスタムフック
+ * @returns 都道府県リスト、ロード状態、エラーメッセージを含むオブジェクト
+ */
+export function usePrefectures() {
+  const prefectures = useSelector((state: RootState) =>
+    selectPrefectures(state),
+  );
+  const loading = useSelector((state: RootState) =>
+    selectPrefecturesLoading(state),
+  );
+  const error = useSelector((state: RootState) =>
+    selectPrefecturesError(state),
+  );
+
+  return {
+    prefectures,
+    loading,
+    error,
+  };
+}
---
+++ population-visualization/src/components/pages/CheckBoxGroup/presenter.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import CheckBoxPresentational, {
+  ContainerProps as CheckBoxPresentationalProps,
+} from './CheckBox/presenter';
+import './CheckBoxGroupPresentational.css';
+import { SelectedPrefecture } from '../../../reducks/selectedPrefecture/types';
+
+export type CheckBoxOption = {
+  label: SelectedPrefecture;
+  checked: boolean;
+};
+
+type Props = {
+  options: CheckBoxOption[];
+  onChange: (label: string, checked: boolean) => void;
+  className?: string;
+};
+
+const CheckBoxGroupPresentational: React.FC<Props> = ({
+  options,
+  onChange,
+  className,
+}) => (
+  <div className={`checkbox-group ${className}`}>
+    {options.map((option) => (
+      <CheckBoxPresentational
+        key={option.label.prefCode}
+        label={option.label}
+        checked={option.checked}
+        onChange={(checked) => onChange(option.label.prefName, checked)}
+        className="checkbox-item"
+      />
+    ))}
+  </div>
+);
+
+export default React.memo(CheckBoxGroupPresentational);
