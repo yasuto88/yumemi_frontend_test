a983f228f8448697e798356084cf141de5e39b79
yasuto88 <yasuto010788@gmail.com>
2024-05-25 16:20:28 +0900

    feat: Add lazy loading for images in Home page

--- population-visualization/src/components/pages/Home/presenter.tsx
+++ population-visualization/src/components/pages/Home/presenter.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './presenter.css';
 import { Header } from '../../uniqueParts/Header';
 import { CheckBoxGroup } from './CheckBoxGroup';
+import { Chart } from '../../uniqueParts/Chart';
 
 export type ContainerProps = {
   className?: string;
@@ -13,7 +14,9 @@ const HomePresentational: React.FC<Props> = ({ className }) => (
   <div className={className}>
     <Header />
     <CheckBoxGroup />
+    <Chart />
   </div>
 );
 
 export default React.memo(HomePresentational);
+
