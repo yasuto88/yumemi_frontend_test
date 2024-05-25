14ed7572d51b867aa701a3746c8ab26d25bfb4da
yasuto88 <yasuto010788@gmail.com>
2024-05-25 11:26:19 +0900

    refactor: Update ESLint configuration and ignore files
    ESLintの設定と無視するファイルを更新

--- population-visualization/eslint.config.mjs
+++ population-visualization/eslint.config.mjs
@@ -10,23 +10,26 @@ const config = [
   {
     languageOptions: {
       parser: tsParser,
-      globals: { ...globals.browser, ...globals.node }
+      globals: { ...globals.browser, ...globals.node },
     },
     rules: {
-      'testing-library/no-node-access': ['error', { allowContainerFirstChild: true }]
+      'testing-library/no-node-access': [
+        'error',
+        { allowContainerFirstChild: true },
+      ],
     },
     plugins: {
       ...pluginJs.configs.recommended.plugins,
       ...tseslint.configs.recommended.plugins,
       ...fixupConfigRules(pluginReactConfig).plugins,
-      'testing-library': testingLibrary
+      'testing-library': testingLibrary,
     },
     settings: {
       ...pluginJs.configs.recommended.settings,
       ...tseslint.configs.recommended.settings,
-      ...fixupConfigRules(pluginReactConfig).settings
-    }
-  }
+      ...fixupConfigRules(pluginReactConfig).settings,
+    },
+  },
 ];
 
 export default config;
